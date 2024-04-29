6b9ff1e9620049437e9fc23155f45bbcd3cada6b
filipejonatas <143852930+filipejonatas@users.noreply.github.com>
2024-04-28 22:36:19 -0300

    montando os 2 cards

--- frontend/src/components/AdmPanel/admPanel.tsx
+++ frontend/src/components/AdmPanel/admPanel.tsx
@@ -3,6 +3,11 @@ import "../AdmPanel/admPanel.css";
 import map from "../../images/AdmPanel/map.png";
 //@ts-ignore
 import satellite from "../../images/AdmPanel/satellite.png"
+//@ts-ignore
+import card1 from "../../images/AdmPanel/card1.png"
+//@ts-ignore
+import card2 from "../../images/AdmPanel/card2.png"
+
 
 export default function AdminPanel() {
   return (
@@ -11,15 +16,28 @@ export default function AdminPanel() {
       <div className="cardcontainer">
         <div className="card1">
           <div className="cardtxt1">
-            <h1>Painel de Projetos</h1>
-            <h2>Acesse seus projetos</h2>
+            <h2>Painel de Projetos</h2>
+            <p>Acesse seus projetos</p>
+            <div className="butadm">
+              <button>Acessar</button>
+            </div>
+          </div>
+          <div className="cardimg1">
+            <img className="img-card1" src={card1} />
           </div>
         </div>
         <div className="card2">
           <div className="cardtxt2">
-          <h1>Painel de usuários</h1>
-          <h2>Acesse o perfil de usuários</h2>
-        </div></div>
+            <h2>Painel de usuários</h2>
+            <p>Acesse o perfil de usuários</p>
+            <div className="butadm">
+              <button>Acessar</button>
+            </div>
+          </div>
+          <div className="cardimg2">
+            <img className="img-card2" src={card2} />
+          </div>
+        </div>
       </div>
       <img className="img-boxsat" src={satellite} />
     </div>
